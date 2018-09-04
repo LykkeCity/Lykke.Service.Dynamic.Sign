0f349817048f9f1b9ee173951f52ea1c22e34337
Amir Abrams <aabrams@myharmoniq.com>
2018-09-03 21:36:33 -0500

    Use Dynamic Nuget package and fix test addresses

--- tests/Lykke.Service.Dynamic.Sign.Tests/DynamicServiceTests.cs
+++ tests/Lykke.Service.Dynamic.Sign.Tests/DynamicServiceTests.cs
@@ -19,11 +19,11 @@ namespace Lykke.Service.Dynamic.Sign.Tests
     {
         private ILog _log;
         public Network network = DynamicNetworks.Testnet;
-        public string from = "ygFX7C2QGD5YQG6EE9wGFddTxqMdUwELuB";
-        public string fromPrivateKey = "cV9nTtEJwgLe7pmSALvzrtQbjtP7zg8phhzDqgEURvWTEmSAVGjH";
+        public string from = "DQ5H7o7WcP9gT3VLZZQW3qeq2r1mmy6SLB";
+        public string fromPrivateKey = "Mj9qy3cFENVjikdvGz8TGi9Xrq7ypNRwSTL19JeZhmyJsswN53zL";
         public BitcoinAddress fromAddress;
         public Key fromKey;
-        public string to = "ybEoFHH7mXyj89trADqP1iYBvU9UyjJdoV";
+        public string to = "D9JitGHNd99zzxhos5qx7ox1kZFikGTyFc";
         public BitcoinAddress toAddress;
         public Transaction prevTx = Transaction.Parse("0100000001e2f23b2d1580d765bd969d1889d4700076688d3219f5686ac21573757f55fff50000000000ffffffff02b0ebffea0f0000001976a914daa46815060c0372118e52ccf970c4c54031055b88ac00e1f505000000001976a9146a4c6d1473b8a5bb8cfea0319d8dac1bc24e147088ac00000000");
         public TransactionBuilder txBuilder = new TransactionBuilder();
